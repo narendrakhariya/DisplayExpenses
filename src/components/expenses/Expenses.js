f0ad8cb7ef789df742ade3d34ab3f3f1ced1b6d5
Narendra Khariya <khariyanaresh007@gmail.com>
2023-01-23 11:11:36 +0900

    Passing value from child to parent component

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import ExpenseItem from "../expense-item/ExpenseItem";
+import ExpensesFilter from "../expenses-filter/ExpensesFilter";
 import "./Expenses.css";
 
 const Expenses = (props) => {
+  const [selectedYear, setSelectedYear] = useState("");
+  const expenseYearOnChange = (expenseYear) => {
+    setSelectedYear(expenseYear);
+  };
   return (
     <div className="expenses">
+      <ExpensesFilter expenseYearOnChange={expenseYearOnChange} />
       {props.expenses.map((expense) => (
         <ExpenseItem
           key={expense.id}
